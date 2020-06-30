35b909f3be3206ef46a66daddaa2632cc92a5ae0
Your Name <you@example.com>
2020-06-30 21:32:57 +0200

    displaying a list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // to add whole programm to index.html
   selector: 'app-root',
+  // to add whole programm to index.html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'myPizza';
+  pizzas: Pizza[] = [
+    { name: 'Margaritha', price: 15 },
+    { name: 'Napoli', price: 18 },
+    { name: 'Hawaii', price: 20 },
+  ];
 }
+
